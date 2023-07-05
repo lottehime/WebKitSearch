a9758282a5fbfe7b6ebcc61d1f9386acb2d6cc93
Lotte <joshdoes3d@gmail.com>
2023-07-05 11:25:20 +1000

    Update webkitsearch.sh

--- webkitsearch.sh
+++ webkitsearch.sh
@@ -2,7 +2,7 @@
 
 #==============================================
 # webkitsearch.sh - webkit git commit searcher
-# Copyright 2014 Josh_Axey
+# Copyright 2014
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
